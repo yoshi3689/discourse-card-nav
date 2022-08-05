a3a8d1f29d208d201f94cb6aa80f1514b7c454ee
Yoshiyuki Nagai <ynagai@my.bcit.ca>
2022-08-05 13:18:55 -0700

    test category links

--- javascripts/discourse/components/card-nav.js
+++ javascripts/discourse/components/card-nav.js
@@ -19,7 +19,7 @@ export default Component.extend({
     component._super(...arguments);
   if (component.onCategories) {
     const categoriesToShow = component.site.categories.map((c, i) => {
-      return i > 8 ? {...c, showByDefault : "card-hidden"} : {...c, showByDefault : ""};
+      return i > 8 ? {...c, showByDefault : "card-hidden", url: `/c/${c.slug}/id`} : {...c, showByDefault : "", url: `/c/${c.slug}/id`};
     });
     categoriesToShow.shift();
     component.set("categories", categoriesToShow);
